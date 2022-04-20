2f734fe57a45c24762e1f9a97e047d15689c269a
ywalakamar <barxwells@gmail.com>
2022-04-20 17:27:07 +0300

    chore(triangle): create triangle class
    - add `triangle.java` file
    - create `Triangle object` in the main file

--- src/com/ywalakamar/di/App.java
+++ src/com/ywalakamar/di/App.java
@@ -2,6 +2,7 @@ package com.ywalakamar.di;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Triangle tri = new Triangle();
+        tri.draw();
     }
 }
